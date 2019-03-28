99b54e53156f6a6ae20b6079c18286fffcd0fb36
matthewbehnke <matthewbehnke0@gmail.com>
2019-03-27 23:05:33 -0500

    Updated gitginore and changed authapi to a package

--- api/authapi/main.go
+++ api/authapi/main.go
@@ -1,65 +1,65 @@
-package main
+package authapi
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"log"
+// 	"os"
 
-	"github.com/gin-contrib/rollbar"
-	"github.com/joho/godotenv"
-	"github.com/stvp/roll"
-)
+// 	"github.com/gin-contrib/rollbar"
+// 	"github.com/joho/godotenv"
+// 	"github.com/stvp/roll"
+// )
 
-const versionNumber = "0.3.0"
+// const versionNumber = "0.3.0"
 
-var (
-	help       bool
-	version    bool
-	portNumber int
-)
+// var (
+// 	help       bool
+// 	version    bool
+// 	portNumber int
+// )
 
-func printHelp() {
-	flag.PrintDefaults()
-	os.Exit(0)
-}
+// func printHelp() {
+// 	flag.PrintDefaults()
+// 	os.Exit(0)
+// }
 
-func printVersion() {
-	fmt.Println(versionNumber)
-	os.Exit(0)
-}
+// func printVersion() {
+// 	fmt.Println(versionNumber)
+// 	os.Exit(0)
+// }
 
-func printArgs() {
-	if version {
-		printVersion()
-	}
-	if help {
-		printHelp()
-	}
-}
+// func printArgs() {
+// 	if version {
+// 		printVersion()
+// 	}
+// 	if help {
+// 		printHelp()
+// 	}
+// }
 
-func init() {
-	flag.BoolVar(&help, "help", false, "Prints out avaliable commands")
-	flag.BoolVar(&version, "version", false, "prints the version number")
-	flag.IntVar(&portNumber, "port", 8080, "set custom port number")
-	flag.Parse()
-}
+// func init() {
+// 	flag.BoolVar(&help, "help", false, "Prints out avaliable commands")
+// 	flag.BoolVar(&version, "version", false, "prints the version number")
+// 	flag.IntVar(&portNumber, "port", 8080, "set custom port number")
+// 	flag.Parse()
+// }
 
-func main() {
-	printArgs()
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+// func main() {
+// 	printArgs()
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
 
-	router := SetupRouter()
+// 	router := SetupRouter()
 
-	roll.Token = os.Getenv("RollBarToken")
-	roll.Endpoint = os.Getenv("RollBarEnv")
-	//roll.Environment = "production" // defaults to "development"
-	router.Use(rollbar.Recovery(true))
-	roll.Info("Starting server", map[string]string{})
+// 	roll.Token = os.Getenv("RollBarToken")
+// 	roll.Endpoint = os.Getenv("RollBarEnv")
+// 	//roll.Environment = "production" // defaults to "development"
+// 	router.Use(rollbar.Recovery(true))
+// 	roll.Info("Starting server", map[string]string{})
 
-	// start the server
-	router.Run(fmt.Sprintf(":%d", portNumber))
-}
+// 	// start the server
+// 	router.Run(fmt.Sprintf(":%d", portNumber))
+// }
